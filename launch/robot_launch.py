8d82c3e619f9df7acc1310152aa742b94e734164
Inmo Jang <inmo3592@gmail.com>
2024-05-24 20:41:34 +0900

    ROSify: Step 3 - Add robot_state_publisher

--- launch/robot_launch.py
+++ launch/robot_launch.py
@@ -4,17 +4,25 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_controller import WebotsController
-
+from launch_ros.actions import Node
 
 def generate_launch_description():
     package_dir = get_package_share_directory('webots_ros2_turtlebot4')
-    
 
     # Start a Webots simulation instance
     webots = WebotsLauncher(
         world=os.path.join(package_dir, 'worlds', 'turtlebot4_world.wbt')
     )
 
+    # Create the robot state publisher
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': '<robot name=""><link name=""/></robot>'
+        }],
+    )
 
     # ROS control spawners
     ros2_control_params = os.path.join(package_dir, 'resource', 'ros2control.yaml')
@@ -24,15 +32,18 @@ def generate_launch_description():
     robot_driver = WebotsController(
         robot_name='Turtlebot4',
         parameters=[
-            {'robot_description': robot_description_path},
+            {'robot_description': robot_description_path,
+             'use_sim_time': True,
+             'set_robot_state_publisher': True
+             },
             ros2_control_params
         ]
     )
 
     return LaunchDescription([
         webots,
+        robot_state_publisher,
         robot_driver,
-
         # The following action will kill all nodes once the Webots simulation has exited
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
