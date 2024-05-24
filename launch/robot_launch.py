03a6ebb7e7d42095ffedc35386148820ed7ff01c
Inmo Jang <inmo3592@gmail.com>
2024-05-24 20:43:14 +0900

    ROSify: Step 4 - Add joint_state_broadcaster (by ros2_control)

--- launch/robot_launch.py
+++ launch/robot_launch.py
@@ -4,6 +4,7 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_controller import WebotsController
+from webots_ros2_driver.wait_for_controller_connection import WaitForControllerConnection
 from launch_ros.actions import Node
 
 def generate_launch_description():
@@ -26,6 +27,14 @@ def generate_launch_description():
 
     # ROS control spawners
     ros2_control_params = os.path.join(package_dir, 'resource', 'ros2control.yaml')
+    controller_manager_timeout = ['--controller-manager-timeout', '50']
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        output='screen',
+        arguments=['joint_state_broadcaster'] + controller_manager_timeout,
+    )    
+    ros_control_spawners = [joint_state_broadcaster_spawner]
 
     # Create a ROS node interacting with the simulated robot
     robot_description_path = os.path.join(package_dir, 'resource', 'turtlebot4.urdf')
@@ -40,10 +49,17 @@ def generate_launch_description():
         ]
     )
 
+    # Wait for the simulation to be ready to start navigation nodes
+    waiting_nodes = WaitForControllerConnection(
+        target_driver=robot_driver,
+        nodes_to_start= ros_control_spawners
+    )    
+
     return LaunchDescription([
         webots,
         robot_state_publisher,
         robot_driver,
+        waiting_nodes,
         # The following action will kill all nodes once the Webots simulation has exited
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
